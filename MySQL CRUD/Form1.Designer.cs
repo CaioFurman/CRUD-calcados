8f63d09ba8f1b913223501e78c637cf5b80d3f97
kns <kannesf@gmail.com>
2021-10-27 17:14:08 -0300

    Add project files.

---
+++ MySQL CRUD/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace MySQL_CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.tbEstoque = new System.Windows.Forms.TextBox();
+            this.tbValor = new System.Windows.Forms.TextBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.dgTabela = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbTipo = new System.Windows.Forms.ComboBox();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgTabela)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 29);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Estoque:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(245, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 29);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Preço R$:";
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(100, 48);
+            this.tbID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(96, 33);
+            this.tbID.TabIndex = 1;
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(293, 48);
+            this.tbNome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(511, 33);
+            this.tbNome.TabIndex = 1;
+            // 
+            // tbEstoque
+            // 
+            this.tbEstoque.Location = new System.Drawing.Point(161, 103);
+            this.tbEstoque.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbEstoque.Name = "tbEstoque";
+            this.tbEstoque.Size = new System.Drawing.Size(77, 33);
+            this.tbEstoque.TabIndex = 1;
+            // 
+            // tbValor
+            // 
+            this.tbValor.Location = new System.Drawing.Point(362, 103);
+            this.tbValor.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbValor.Name = "tbValor";
+            this.tbValor.Size = new System.Drawing.Size(143, 33);
+            this.tbValor.TabIndex = 1;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnInserir.Location = new System.Drawing.Point(53, 164);
+            this.btnInserir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(145, 37);
+            this.btnInserir.TabIndex = 2;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnConsultar.Location = new System.Drawing.Point(508, 164);
+            this.btnConsultar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(145, 37);
+            this.btnConsultar.TabIndex = 2;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnAlterar.Location = new System.Drawing.Point(205, 164);
+            this.btnAlterar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(145, 37);
+            this.btnAlterar.TabIndex = 2;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // dgTabela
+            // 
+            this.dgTabela.AllowUserToAddRows = false;
+            this.dgTabela.AllowUserToDeleteRows = false;
+            this.dgTabela.AllowUserToResizeColumns = false;
+            this.dgTabela.AllowUserToResizeRows = false;
+            this.dgTabela.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgTabela.Location = new System.Drawing.Point(53, 230);
+            this.dgTabela.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgTabela.Name = "dgTabela";
+            this.dgTabela.ReadOnly = true;
+            this.dgTabela.Size = new System.Drawing.Size(752, 419);
+            this.dgTabela.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(511, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 29);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Tipo:";
+            // 
+            // cbTipo
+            // 
+            this.cbTipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTipo.FormattingEnabled = true;
+            this.cbTipo.Items.AddRange(new object[] {
+            "Botas",
+            "Chinelos",
+            "Chuteiras",
+            "Sapatênis",
+            "Sapatilhas",
+            "Tênis",
+            "Tênis de Corrida"});
+            this.cbTipo.Location = new System.Drawing.Point(586, 103);
+            this.cbTipo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbTipo.Name = "cbTipo";
+            this.cbTipo.Size = new System.Drawing.Size(219, 37);
+            this.cbTipo.TabIndex = 4;
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnExibir.Location = new System.Drawing.Point(660, 164);
+            this.btnExibir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(145, 37);
+            this.btnExibir.TabIndex = 2;
+            this.btnExibir.Text = "Atualizar";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnDeletar.Location = new System.Drawing.Point(358, 164);
+            this.btnDeletar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(145, 37);
+            this.btnDeletar.TabIndex = 2;
+            this.btnDeletar.Text = "Excluir";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(318, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(227, 29);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Gestão de Produtos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 674);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbTipo);
+            this.Controls.Add(this.dgTabela);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.tbValor);
+            this.Controls.Add(this.tbEstoque);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestão de Produtos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgTabela)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.TextBox tbEstoque;
+        private System.Windows.Forms.TextBox tbValor;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.DataGridView dgTabela;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbTipo;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ MySQL CRUD/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MySQL_CRUD
+{
+    public partial class Form1 : Form
+    {
+        
+        MySqlConnection cn;
+        MySqlCommand cmd;
+        MySqlDataAdapter da;
+        MySqlDataReader dr;
+        string strSQL;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        //INSERIR
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cn = new MySqlConnection(@"server=localhost;user id=root;password=1234;database=loja_calcados;");
+                strSQL = "insert into loja_calcados (Nome, Estoque, Valor, Tipo) values (@Nome, @Estoque, @Valor, @Tipo)";
+                cmd = new MySqlCommand(strSQL, cn);
+
+                cmd.Parameters.AddWithValue("@Nome", tbNome.Text);
+                cmd.Parameters.AddWithValue("@Estoque", tbEstoque.Text);
+                cmd.Parameters.AddWithValue("@Valor", tbValor.Text);
+                cmd.Parameters.AddWithValue("@Tipo", cbTipo.Text);
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                cn = null;
+                cmd = null;
+            }
+
+            Limpar();
+        }
+        
+        //ALTERAR
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cn = new MySqlConnection(@"server=localhost;user id=root;password=1234;database=loja_calcados;");
+                strSQL = "update loja_calcados set Nome = @Nome, Estoque = @Estoque, Valor = @Valor, Tipo = @Tipo where ID = @ID";
+                cmd = new MySqlCommand(strSQL, cn);
+
+                cmd.Parameters.AddWithValue("@ID", tbID.Text);
+                cmd.Parameters.AddWithValue("@Nome", tbNome.Text);
+                cmd.Parameters.AddWithValue("@Estoque", tbEstoque.Text);
+                cmd.Parameters.AddWithValue("@Valor", tbValor.Text);
+                cmd.Parameters.AddWithValue("@Tipo", cbTipo.Text);
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                cn = null;
+                cmd = null;
+            }
+
+            Limpar();
+        }
+
+        //DELETAR
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cn = new MySqlConnection(@"server=localhost;user id=root;password=1234;database=loja_calcados;");
+                strSQL = "delete from loja_calcados where ID = @ID";
+                cmd = new MySqlCommand(strSQL, cn);
+
+                cmd.Parameters.AddWithValue("@ID", tbID.Text);
+                cmd.Parameters.AddWithValue("@Nome", tbNome.Text);
+                cmd.Parameters.AddWithValue("@Estoque", tbEstoque.Text);
+                cmd.Parameters.AddWithValue("@Valor", tbValor.Text);
+                cmd.Parameters.AddWithValue("@Tipo", cbTipo.Text);
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                cn = null;
+                cmd = null;
+            }
+
+            Limpar();
+        }
+
+        //CONSULTAR
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cn = new MySqlConnection(@"server=localhost;user id=root;password=1234;database=loja_calcados;");
+                strSQL = "select * from loja_calcados where ID = @ID";
+                cmd = new MySqlCommand(strSQL, cn);
+
+                cmd.Parameters.AddWithValue("@ID", tbID.Text);
+                cmd.Parameters.AddWithValue("@Nome", tbNome.Text);
+                cmd.Parameters.AddWithValue("@Estoque", tbEstoque.Text);
+                cmd.Parameters.AddWithValue("@Valor", tbValor.Text);
+                cmd.Parameters.AddWithValue("@Tipo", cbTipo.Text);
+
+                cn.Open();
+
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    tbNome.Text = Convert.ToString(dr["Nome"]);
+                    tbEstoque.Text = Convert.ToString(dr["Estoque"]);
+                    tbValor.Text = Convert.ToString(dr["Valor"]);
+                    cbTipo.Text = Convert.ToString(dr["Tipo"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                cn = null;
+                cmd = null;
+            }
+
+        }
+
+        //EXIBIR
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cn = new MySqlConnection(@"server=localhost;user id=root;password=1234;database=loja_calcados;");
+                strSQL = "select * from loja_calcados";
+
+                da = new MySqlDataAdapter(strSQL, cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgTabela.DataSource = dt;
+                
+                ConfigurarTabela();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+                cn = null;
+                cmd = null;
+            }
+        }
+
+        private void ConfigurarTabela()
+        {
+            dgTabela.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 12F);
+            dgTabela.ColumnHeadersDefaultCellStyle.Font = new Font("Microsoft Sans Serif", 14F);
+            dgTabela.RowHeadersWidth = 25;
+
+            //ID
+            dgTabela.Columns["ID"].HeaderText = "ID";
+            dgTabela.Columns["ID"].Width = 50;
+            dgTabela.Columns["ID"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgTabela.Columns["ID"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            //Nome
+            dgTabela.Columns["Nome"].HeaderText = "Nome";
+            dgTabela.Columns["Nome"].Width = 325;
+
+            //Tipo
+            dgTabela.Columns["Tipo"].HeaderText = "Tipo";
+            dgTabela.Columns["Tipo"].Width = 150;
+
+            //Preço
+            dgTabela.Columns["Valor"].HeaderText = "Preço";
+            dgTabela.Columns["Valor"].Width = 100;
+            dgTabela.Columns["Valor"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgTabela.Columns["Valor"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+
+            //Estoque
+            dgTabela.Columns["Estoque"].HeaderText = "Estoque";
+            dgTabela.Columns["Estoque"].Width = 100;
+            dgTabela.Columns["Estoque"].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgTabela.Columns["Estoque"].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        }
+
+        private void Limpar()
+        {
+            tbEstoque.Clear();
+            tbID.Clear();
+            tbNome.Clear();
+            tbValor.Clear();
+        }
+
+    }
+}
